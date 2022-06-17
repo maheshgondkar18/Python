a125d7ab460f9a648532032b7b423ae63a0c6a3a
maheshgondkar18 <gondkarmahesh3@gmail.com>
2022-06-17 15:35:50 +0530

    Exception Handling,Iterator, Generator

---
+++ Day4/Iterator.py
@@ -0,0 +1,31 @@
+
+class Test:
+
+
+	def __init__(self, limit):
+		self.limit = limit
+
+	def __iter__(self):
+		self.x = 10
+		return self
+
+
+	def __next__(self):
+
+
+		x = self.x
+
+
+		if x > self.limit:
+			raise StopIteration
+
+
+		self.x = x + 1;
+		return x
+
+for i in Test(15):
+	print(i)
+
+# Prints nothing
+for i in Test(5):
+	print(i)
